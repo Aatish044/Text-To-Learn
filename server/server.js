a1520947fe6c080d3c6e606bb86c9cc3240bc02e
Aatish044 <ms9891128119@gmail.com>
2026-07-10 15:54:22 +0530

    cors fix

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 app.use(cors({
-  origin: ["http://localhost:5173", "https://text-to-learn.vercel.app"],
+  origin: ["http://localhost:5173", "https://text-to-learn-one.vercel.app"],
   credentials: true,
 }));
 
